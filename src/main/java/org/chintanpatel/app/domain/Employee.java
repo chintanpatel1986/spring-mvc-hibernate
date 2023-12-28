e63801380f9889183b96eaaa29a53f7db14cd25d
chintanpatel <chintanpatel2013@icloud.com>
2023-12-28 18:18:28 +0530

    Update Employee.java and messages.properties file for crud application.

--- src/main/java/org/chintanpatel/app/domain/Employee.java
+++ src/main/java/org/chintanpatel/app/domain/Employee.java
@@ -3,7 +3,6 @@ package org.chintanpatel.app.domain;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import org.springframework.format.annotation.DateTimeFormat;
 
